325d82169d97735e9ea2f328d9d824071fd3863e
Bagus Setyo <bagussetyo442@gmail.com>
2023-11-11 09:24:20 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<script setup>
+import NavNavigation from "./components/NavNavigation.vue";
+</script>
+
+<template>
+  <NavNavigation />
+  <div class="app sm:flex block h-screen p-12">
+    <div class="w-2/3 bg-transparent p-4">
+      <h1 class="text-7xl font-bold mt-10 mb-5">
+        Make <br />
+        remote work
+      </h1>
+      <p class="text-gray-500">
+        Get your team in sync, no matter your location. <br />
+        Streamline processes, create team rituals, and <br />
+        watch productivity soar.
+      </p>
+      <div class="mt-10">
+        <button
+          class="bg-black font-bold text-white py-3 px-4 rounded-xl hover:bg-white hover:border hover:border-black hover:text-black"
+          type=""
+        >
+          Learn More
+        </button>
+      </div>
+      <div class="flex mt-10 justify-evenly">
+        <img class="h-fit" src="/images/client-databiz.svg" alt="" />
+        <img class="h-fit" src="/images/client-audiophile.svg" alt="" />
+        <img class="h-fit" src="/images/client-meet.svg" alt="" />
+        <img class="h-fit" src="/images/client-maker.svg" alt="" />
+      </div>
+    </div>
+    <div class="w-1/3 bg-transparent">
+      <img
+        class="max-w-xs w-full"
+        src="/images/image-hero-desktop.png"
+        alt=""
+      />
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/NavNavigation.vue
@@ -0,0 +1,216 @@
+<script setup>
+import {
+  Disclosure,
+  DisclosureButton,
+  DisclosurePanel,
+  Menu,
+  MenuButton,
+  MenuItems,
+  MenuItem,
+  TransitionRoot,
+} from "@headlessui/vue";
+import { ref } from "vue";
+import {
+  ChevronDownIcon,
+  ChevronUpIcon,
+  Bars3Icon,
+} from "@heroicons/vue/20/solid";
+
+const navigation = [
+  { name: "Dashboard", href: "#", current: true },
+  { name: "Team", href: "#", current: false },
+  { name: "Projects", href: "#", current: false },
+  { name: "Calendar", href: "#", current: false },
+];
+const showDropdownFeatures = ref(false);
+const showDropdownCompany = ref(false);
+
+const toggleDropdownFeatures = () => {
+  showDropdownFeatures.value = !showDropdownFeatures.value;
+  showDropdownCompany.value = false;
+};
+
+const toggleDropdownCompany = () => {
+  showDropdownCompany.value = !showDropdownCompany.value;
+  showDropdownFeatures.value = false;
+};
+
+// logo Sponsor
+const features = [
+  { img: "/images/icon-todo.svg", name: "Todo List" },
+  { img: "/images/icon-calendar.svg", name: "Calendar" },
+  { img: "/images/icon-reminders.svg", name: "Reminders" },
+  { img: "/images/icon-planning.svg", name: "Planning" },
+];
+</script>
+
+<template>
+  <Disclosure as="nav" class="bg-transparent">
+    <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+      <div class="relative flex h-16 items-center justify-between">
+        <div class="absolute inset-y-0 right-0 flex items-center sm:hidden">
+          <!-- Mobile menu button-->
+          <DisclosureButton
+            class="relative inline-flex items-center justify-center rounded-md p-2 text-black hover:bg-black hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+          >
+            <span class="absolute -inset-0.5" />
+            <span class="sr-only">Open main menu</span>
+            <Bars3Icon class="block h-6 w-6" aria-hidden="true" />
+          </DisclosureButton>
+        </div>
+        <div
+          class="flex flex-1 items-center sm:items-stretch sm:ml-0 ml-4 justify-start"
+        >
+          <div class="flex flex-shrink-0 items-center">
+            <img class="h-4 mr-5 w-auto" src="/images/logo.svg" alt="Snap" />
+          </div>
+          <div class="hidden sm:ml-6 sm:block">
+            <div class="flex space-x-4">
+              <Menu as="div" class="relative text-left">
+                <MenuButton
+                  v-on:click="toggleDropdownFeatures"
+                  :class="{ 'text-black': showDropdownFeatures }"
+                  class="inline-flex w-full justify-center rounded-m px-4 py-2 text-sm font-medium text-gray-500 hover:text-black"
+                >
+                  Features
+                  <ChevronDownIcon
+                    :class="{ 'rotate-180 text-black': showDropdownFeatures }"
+                    class="ml-2 -mr-1 h-5 w-5 transition-transform transform text-gray-500 hover:text-black"
+                    aria-hidden="true"
+                  />
+                </MenuButton>
+
+                <transition
+                  enter-active-class="transition duration-100 ease-out"
+                  enter-from-class="transform scale-95 opacity-0"
+                  enter-to-class="transform scale-100 opacity-100"
+                  leave-active-class="transition duration-75 ease-in"
+                  leave-from-class="transform scale-100 opacity-100"
+                  leave-to-class="transform scale-95 opacity-0"
+                >
+                  <MenuItems
+                    class="absolute right-0 mt-2 w-56 origin-top-right rounded-lg bg-white shadow-lg"
+                  >
+                    <div class="px-1 py-1">
+                      <MenuItem
+                        v-for="(feature, index) in features"
+                        :key="index"
+                      >
+                        <button
+                          class="group flex w-full items-center rounded-md px-2 py-2 text-sm"
+                        >
+                          <img class="mx-5" :src="feature.img" />
+                          <p class="font-thin text-gray-600">
+                            {{ feature.name }}
+                          </p>
+                        </button>
+                      </MenuItem>
+                    </div>
+                  </MenuItems>
+                </transition>
+              </Menu>
+              <Menu as="div" class="relative text-left">
+                <MenuButton
+                  v-on:click="toggleDropdownCompany"
+                  :class="{ 'text-black': showDropdownCompany }"
+                  class="inline-flex w-full justify-center rounded-m px-4 py-2 text-sm font-medium text-gray-500 hover:text-black"
+                >
+                  Company
+                  <ChevronDownIcon
+                    :class="{ 'rotate-180 text-black': showDropdownCompany }"
+                    class="ml-2 -mr-1 h-5 w-5 transition-transform transform text-gray-500 hover:text-black"
+                    aria-hidden="true"
+                  />
+                </MenuButton>
+
+                <transition
+                  enter-active-class="transition duration-100 ease-out"
+                  enter-from-class="transform scale-95 opacity-0"
+                  enter-to-class="transform scale-100 opacity-100"
+                  leave-active-class="transition duration-75 ease-in"
+                  leave-from-class="transform scale-100 opacity-100"
+                  leave-to-class="transform scale-95 opacity-0"
+                >
+                  <MenuItems
+                    class="absolute right-0 mt-2 w-32 origin-top-right rounded-lg bg-white shadow-lg"
+                  >
+                    <div class="px-1 py-1">
+                      <MenuItem>
+                        <button
+                          class="group flex w-full items-center rounded-md px-2 py-2 text-sm"
+                        >
+                          <p class="font-thin text-gray-600">History</p>
+                        </button>
+                      </MenuItem>
+                      <MenuItem>
+                        <button
+                          class="group flex w-full items-center rounded-md px-2 py-2 text-sm"
+                        >
+                          <p class="font-thin text-gray-600">Our Team</p>
+                        </button>
+                      </MenuItem>
+                      <MenuItem>
+                        <button
+                          class="group flex w-full items-center rounded-md px-2 py-2 text-sm"
+                        >
+                          <p class="font-thin text-gray-600">Blog</p>
+                        </button>
+                      </MenuItem>
+                    </div>
+                  </MenuItems>
+                </transition>
+              </Menu>
+              <div
+                class="relative text-left w-full items-center rounded-md px-2 py-2 text-sm cursor-pointer"
+              >
+                <p class="font-thin text-gray-600 hover:text-black">Careers</p>
+              </div>
+              <div
+                class="relative text-left w-full items-center rounded-md px-2 py-2 text-sm cursor-pointer"
+              >
+                <p class="font-thin text-gray-600 hover:text-black">About</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div
+          class="absolute inset-y-0 right-0 sm:flex hidden items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0"
+        >
+          <button
+            type="button"
+            class="font-thin bg-transparent p-5 mr-5 text-gray-500 hover:text-black"
+          >
+            Login
+          </button>
+          <button
+            type="button"
+            class="font-thin border border-grey-500 rounded-lg bg-transparent p-4 text-gray-500 hover:text-black hover:border-black"
+          >
+            Register
+          </button>
+        </div>
+      </div>
+    </div>
+
+    <DisclosurePanel class="sm:hidden">
+      <div class="relative space-y-1 px-2 pb-3 pt-2">
+        <DisclosureButton
+          v-for="item in navigation"
+          :key="item.name"
+          as="a"
+          :href="item.href"
+          :class="[
+            item.current
+              ? 'bg-gray-900 text-white'
+              : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+            'block rounded-md px-3 py-2 text-base font-medium',
+          ]"
+          :aria-current="item.current ? 'page' : undefined"
+          >{{ item.name }}</DisclosureButton
+        >
+      </div>
+    </DisclosurePanel>
+  </Disclosure>
+</template>
+
+<style></style>
